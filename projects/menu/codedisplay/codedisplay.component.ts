a3ff27be161a250683a271af5b2da3b00a651a7e
swethamura <67194052+swethamura@users.noreply.github.com>
2020-07-28 15:38:24 +0530

    Add files via upload

---
+++ projects/menu/codedisplay/codedisplay.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-codedisplay',
+  templateUrl: './codedisplay.component.html',
+  styleUrls: ['./codedisplay.component.css']
+})
+export class CodedisplayComponent implements OnInit {
+
+  ishtml=true;
+  ists=false;
+
+  displayhtml(){
+    this.ishtml=true;
+    this.ists=false;
+  }
+
+ 
+  displayts(){
+    this.ishtml=false;
+    this.ists=true;
+    }
+ 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
